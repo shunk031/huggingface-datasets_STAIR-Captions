4d37e25aa3881c418e75f72c6bb2faf370e116d1
Shunsuke KITADA <shunsuke.kitada.0831@gmail.com>
2024-06-21 12:31:50 +0900

    [WIP] update files

--- STAIR-Captions.py
+++ STAIR-Captions.py
@@ -17,12 +17,17 @@
 # TODO: Address all TODOs and remove all explanatory comments
 import json
 import os
+from dataclasses import dataclass
 from typing import List
 
 import datasets as ds
 from datasets.utils.logging import get_logger
+from hfcocoapi.processors import CaptionsProcessor
+from hfcocoapi.typehint import JsonDict, LicenseId
+from PIL.Image import Image
 
 logger = get_logger(__name__)
+
 
 _CITATION = """\
 @inproceedings{yoshikawa2017stair,
@@ -34,50 +39,76 @@ _CITATION = """\
 }
 """
 
-# TODO: Add description of the dataset here
-# You can copy an official description
 _DESCRIPTION = """\
 STAIR Captions is a large-scale dataset containing 820,310 Japanese captions.
 """
 
-# TODO: Add a link to an official homepage for the dataset here
 _HOMEPAGE = "http://captions.stair.center/"
 
-# TODO: Add the licence for the dataset here if you can find it
 _LICENSE = "Creative Commons Attribution 4.0 License."
 
-# TODO: Add link to the official dataset URLs here
-# The HuggingFace Datasets library doesn't host the datasets but only points to the original files.
-# This can be an arbitrary nested dict/list of URLs (see below in `_split_generators` method)
 _URLS = {
-    "v1.0.0": {
-        "train": "https://github.com/STAIR-Lab-CIT/STAIR-captions/raw/991f2c4c1168755b5c10ab64174989958223082b/stair_captions_v1.0_train.json",
-        "validation": "https://github.com/STAIR-Lab-CIT/STAIR-captions/raw/991f2c4c1168755b5c10ab64174989958223082b/stair_captions_v1.0_val.json",
+    "annotations": {
+        "v1.0.0": {
+            "train": "https://github.com/STAIR-Lab-CIT/STAIR-captions/raw/991f2c4c1168755b5c10ab64174989958223082b/stair_captions_v1.0_train.json",
+            "validation": "https://github.com/STAIR-Lab-CIT/STAIR-captions/raw/991f2c4c1168755b5c10ab64174989958223082b/stair_captions_v1.0_val.json",
+        },
+        "v1.1.0": {
+            "train": "https://github.com/STAIR-Lab-CIT/STAIR-captions/raw/deaaa3f5198f9bbedc1c090244e25d8305d3265f/stair_captions_v1.1_train.json.tar.gz",
+            "validation": "https://github.com/STAIR-Lab-CIT/STAIR-captions/raw/deaaa3f5198f9bbedc1c090244e25d8305d3265f/stair_captions_v1.1_val.json.tar.gz",
+        },
+        "v1.2.0": "https://github.com/STAIR-Lab-CIT/STAIR-captions/raw/b888953f398cb058ab6e6244cf8ccbe4cceb47f4/stair_captions_v1.2.tar.gz",
     },
-    "v1.1.0": {
-        "train": "https://github.com/STAIR-Lab-CIT/STAIR-captions/raw/deaaa3f5198f9bbedc1c090244e25d8305d3265f/stair_captions_v1.1_train.json.tar.gz",
-        "validation": "https://github.com/STAIR-Lab-CIT/STAIR-captions/raw/deaaa3f5198f9bbedc1c090244e25d8305d3265f/stair_captions_v1.1_val.json.tar.gz",
+    "images": {
+        "train": "http://images.cocodataset.org/zips/train2014.zip",
+        "validation": "http://images.cocodataset.org/zips/val2014.zip",
     },
-    "v1.2.0": "https://github.com/STAIR-Lab-CIT/STAIR-captions/raw/b888953f398cb058ab6e6244cf8ccbe4cceb47f4/stair_captions_v1.2.tar.gz",
 }
 
 
+# class StairCaptionsProcessor(MsCocoProcessor):
+#     pass
+
+
+@dataclass
+class StairCaptionsConfig(ds.BuilderConfig):
+    is_tokenized: bool = False
+
+    def __post_init__(self) -> None:
+        if self.name == "v1.0.0":
+            assert (
+                not self.is_tokenized
+            ), "Tokenized captions are only available for v1.1.0 and v1.2.0"
+
+        elif self.name == "v1.1.0":
+            assert self.is_tokenized, "v1.1.0 should be tokenized"
+
+        elif self.name == "v1.2.0":
+            pass
+        else:
+            raise ValueError(f"Invalid configuration version: {self.name}")
+
+
 # TODO: Name of the dataset usually matches the script name with CamelCase instead of snake_case
-class STAIRCaptionsDataset(ds.GeneratorBasedBuilder):
+class StairCaptionsDataset(ds.GeneratorBasedBuilder):
     """A class for loading STAIR-Captions dataset."""
 
+    BUILDER_CONFIG_CLASS = StairCaptionsConfig
+
     BUILDER_CONFIGS = [
-        ds.BuilderConfig(
+        StairCaptionsConfig(
             name="v1.0.0",
             version=ds.Version("1.0.0"),
             description="Initial version of the dataset",
+            is_tokenized=False,
         ),
-        ds.BuilderConfig(
+        StairCaptionsConfig(
             name="v1.1.0",
-            version="1.1.0",
+            version=ds.Version("1.1.0"),
             description="Added tokenized captions",
+            is_tokenized=True,
         ),
-        ds.BuilderConfig(
+        StairCaptionsConfig(
             name="v1.2.0",
             version=ds.Version("1.2.0"),
             description="Cleaned up the dataset",
@@ -110,62 +141,77 @@ class STAIRCaptionsDataset(ds.GeneratorBasedBuilder):
     def _split_generators(
         self, dl_manager: ds.DownloadManager
     ) -> List[ds.SplitGenerator]:
-        file_paths = dl_manager.download_and_extract(_URLS[self.config.name])
+        img_file_paths = dl_manager.download_and_extract(
+            url_or_urls=_URLS["images"],
+        )
+        ann_file_paths = dl_manager.download_and_extract(
+            url_or_urls=_URLS["annotations"][self.config.name],  # type: ignore
+        )
 
-        breakpoint()
+        def get_tng_filepath(file_paths) -> str:
+            if self.config.name == "v1.0.0":
+                return file_paths["train"]
 
-        # TODO: This method is tasked with downloading/extracting the data and defining the splits depending on the configuration
-        # If several configurations are possible (listed in BUILDER_CONFIGS), the configuration selected by the user is in self.config.name
+            elif self.config.name == "v1.1.0":
+                return os.path.join(
+                    file_paths["train"], "stair_captions_v1.1_train.json"
+                )
+            elif self.config.name == "v1.2.0":
+                filename = (
+                    "stair_captions_v1.2_train_tokenized.json"
+                    if self.config.is_tokenized  # type: ignore
+                    else "stair_captions_v1.2_train.json"
+                )
+                return os.path.join(file_paths, filename)
 
-        # dl_manager is a datasets.download.DownloadManager that can be used to download and extract URLS
-        # It can accept any type or nested list/dict and will give back the same structure with the url replaced with path to local files.
-        # By default the archives will be extracted and a path to a cached folder where they are extracted is returned instead of the archive
-        urls = _URLS[self.config.name]
-        data_dir = dl_manager.download_and_extract(urls)
+            else:
+                raise ValueError(f"Invalid configuration name: {self.config.name}")
+
+        def get_val_filepath(file_paths) -> str:
+            if self.config.name == "v1.0.0":
+                return file_paths["validation"]
+
+            elif self.config.name == "v1.1.0":
+                return os.path.join(
+                    file_paths["validation"], "stair_captions_v1.1_val.json"
+                )
+            elif self.config.name == "v1.2.0":
+                filename = (
+                    "stair_captions_v1.2_val_tokenized.json"
+                    if self.config.is_tokenized  # type: ignore
+                    else "stair_captions_v1.2_val.json"
+                )
+                return os.path.join(file_paths, filename)
+
+            else:
+                raise ValueError(f"Invalid configuration name: {self.config.name}")
+
         return [
             ds.SplitGenerator(
                 name=ds.Split.TRAIN,  # type: ignore
-                # These kwargs will be passed to _generate_examples
                 gen_kwargs={
-                    "filepath": os.path.join(data_dir, "train.jsonl"),
-                    "split": "train",
+                    "ann_filepath": get_tng_filepath(ann_file_paths),
+                    "img_filepath": img_file_paths["train"],  # type: ignore
                 },
             ),
             ds.SplitGenerator(
                 name=ds.Split.VALIDATION,  # type: ignore
-                # These kwargs will be passed to _generate_examples
                 gen_kwargs={
-                    "filepath": os.path.join(data_dir, "dev.jsonl"),
-                    "split": "dev",
+                    "ann_filepath": get_val_filepath(ann_file_paths),
+                    "img_filepath": img_file_paths["validation"],  # type: ignore
                 },
             ),
         ]
 
     # method parameters are unpacked from `gen_kwargs` as given in `_split_generators`
-    def _generate_examples(self, filepath, split):
-        # TODO: This method handles input defined in _split_generators to yield (key, example) tuples from the dataset.
-        # The `key` is for legacy reasons (tfds) and is not important in itself, but must be unique for each example.
-        with open(filepath, encoding="utf-8") as f:
-            for key, row in enumerate(f):
-                data = json.loads(row)
-                if self.config.name == "first_domain":
-                    # Yields examples as (key, example) tuples
-                    yield (
-                        key,
-                        {
-                            "sentence": data["sentence"],
-                            "option1": data["option1"],
-                            "answer": "" if split == "test" else data["answer"],
-                        },
-                    )
-                else:
-                    yield (
-                        key,
-                        {
-                            "sentence": data["sentence"],
-                            "option2": data["option2"],
-                            "second_domain_answer": (
-                                "" if split == "test" else data["second_domain_answer"]
-                            ),
-                        },
-                    )
+    def _generate_examples(self, filepath: str):
+        processor = CaptionsProcessor()
+        ann_json = processor.load_annotation_json(ann_file_path=filepath)
+
+        licenses = processor.load_licenses_data(license_dicts=ann_json["licenses"])
+        images = processor.load_images_data(image_dicts=ann_json["images"])
+        annotations = processor.load_data(
+            ann_dicts=ann_json["annotations"], images=images
+        )
+
+        breakpoint()
--- tests/STAIR-Captions_test.py
+++ tests/STAIR-Captions_test.py
@@ -14,6 +14,34 @@ def dataset_path(dataset_name: str) -> str:
     return f"{dataset_name}.py"
 
 
+# @pytest.mark.skipif(
+#     condition=bool(os.environ.get("CI", False)),
+#     reason=(
+#         "Because this loading script downloads a large dataset, "
+#         "we will skip running it on CI."
+#     ),
+# )
+# @pytest.mark.parametrize(
+#     argnames="version, is_tokenized",
+#     argvalues=(
+#         ("v1.0.0", False),
+#         ("v1.1.0", True),
+#         ("v1.2.0", False),
+#         ("v1.2.0", True),
+#     ),
+# )
+# def test_load_dataset(dataset_path: str, version: str, is_tokenized: bool):
+#     dataset = ds.load_dataset(
+#         path=dataset_path,
+#         name=version,
+#         is_tokenized=is_tokenized,
+#         trust_remote_code=True,
+#     )
+#     assert isinstance(dataset, ds.DatasetDict)
+
+#     # dataset.push_to_hub(repo_id=repo_id, private=True)
+
+
 @pytest.mark.skipif(
     condition=bool(os.environ.get("CI", False)),
     reason=(
@@ -21,9 +49,18 @@ def dataset_path(dataset_name: str) -> str:
         "we will skip running it on CI."
     ),
 )
-@pytest.mark.parametrize("version", ["v1.0.0", "v1.1.0", "v1.2.0"])
-def test_load_dataset(dataset_path: str, version: str):
-    dataset = ds.load_dataset(path=dataset_path, name=version, trust_remote_code=True)
-    assert isinstance(dataset, ds.DatasetDict)
-
-    # dataset.push_to_hub(repo_id=repo_id, private=True)
+@pytest.mark.parametrize(
+    argnames="version, is_tokenized",
+    argvalues=(
+        ("v1.0.0", True),
+        ("v1.1.0", False),
+    ),
+)
+def test_load_dataset_exception(dataset_path: str, version: str, is_tokenized: bool):
+    with pytest.raises(AssertionError):
+        ds.load_dataset(
+            path=dataset_path,
+            name=version,
+            is_tokenized=is_tokenized,
+            # trust_remote_code=True,
+        )
